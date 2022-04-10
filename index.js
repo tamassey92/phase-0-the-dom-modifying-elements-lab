49529f85fc2e113b260363f99dc4011e23bcb433
Taylor Massey <tamassey92@gmail.com>
2022-04-10 14:49:41 -0600

    commit tree 49a85a65b281d0cea91d0f93e121944994989c66

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const removeMain = document.getElementById('main');
+removeMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+
+newHeader.textContent = 'Taylor is the champion!';
